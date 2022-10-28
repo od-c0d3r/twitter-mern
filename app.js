19cb33c7230b287cdab5bc810f6f862cca2652f7
Omar R <o.rashad2020@gmail.com>
2022-10-28 18:18:08 +0200

    Add users tweets routes

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
 
 mongoose
 .connect(db, { useNewUrlParser: true })
 .then(() => console.log("Connected to MongoDB successfully"))
 .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Sheeesh 2"));
+app.get("/", (req, res) => res.send("Hello DevOmarRashad"));
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
